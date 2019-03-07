2e1c1ef11d22a179d9a89b28dc1a508daf4786f8
HaloSeventy7 <njmalenfant@gmail.com>
2019-03-07 18:06:03 -0500

    Added Title Screen Nav Script

---
+++ Project Dungeon/Assets/Scipts/TitleScreenNav.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] string levelToLoad = "";
+    [SerializeField] Button startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
