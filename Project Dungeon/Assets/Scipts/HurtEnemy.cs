4e5613ba4bea578134a5292cac9cdaedad330e53
HaloSeventy7 <njmalenfant@gmail.com>
2019-03-12 01:39:15 -0400

    Worked on damage system

--- Project Dungeon/Assets/Scipts/HurtEnemy.cs
+++ Project Dungeon/Assets/Scipts/HurtEnemy.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class HurtEnemy : MonoBehaviour
 {
+    [SerializeField] int damageToDeal;
+    [SerializeField] GameObject damageBurst;
+    [SerializeField] Transform hitPoint;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Enemy")
         {
-            Destroy(other.gameObject);
+            //Destroy(other.gameObject);
+            other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToDeal);
+            GameObject tempBurst = Instantiate(damageBurst, other.transform.position, other.transform.rotation);
+            Object.Destroy(tempBurst, 1.0f);
         }
     }
 }
