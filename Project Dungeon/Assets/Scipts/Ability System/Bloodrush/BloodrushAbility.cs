46a0870e5b5b79054842eb2a9579fc111904b429
HaloSeventy7 <njmalenfant@gmail.com>
2019-04-22 00:32:26 -0400

    Created the foundation for the skill system!
    Got more work to do before pushing

---
+++ Project Dungeon/Assets/Scipts/Ability System/Bloodrush/BloodrushAbility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilities/Bloodrush")]
+public class BloodrushAbility : Ability
+{
+    //variables
+
+    public override void Initialize(GameObject obj)
+    {
+        
+    }
+
+    public override void TriggerAbility()
+    {
+        
+    }
+}
---
+++ Project Dungeon/Assets/Scipts/Unity Skill System Tutorial/PracticeLog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//The actual ability
+public class PracticeLog : MonoBehaviour
+{
+    [HideInInspector] public int pretendDamage = 1; //hidden in inspector because we don't want these values to be changed in the inspector, only those from "PracticeAbility"
+    [HideInInspector] public float pretendRange = 50f; //still accessible from other classes
+    [HideInInspector] public float pretendHitForce = 100f;
+
+    public void Initialize()
+    {
+        Debug.Log("Practice Ability Initialized!"); //normally would be done in the Start() function, but doesn't need to be anymore because it's called in "Ability"
+    }
+
+    public void DoSomething() //normally would be checked in the Update() function, but now can be called from "PracticeAbility"
+    {
+        Debug.Log("I did a thing!");
+    }
+}
