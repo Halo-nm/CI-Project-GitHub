5c3af4828446b3c3b0b57296234bfaebe2c83024
Maxwell Sullivan <mgs85@drexel.edu>
2019-05-01 09:30:12 -0400

    Spikes

---
+++ Project Dungeon/Assets/Scipts/MaxPressurePlate1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxPressurePlate1 : MonoBehaviour
+{
+    //[SerializeField] float triggerWaitTime = 1; //in seconds
+    //float timeSinceLastTrigger;
+
+    [SerializeField] Spike SpikeToChange;
+    [SerializeField] Spike SpikeToChange2;
+
+    //[SerializeField] bool spawnObject = false; //set to true if an object should be spawned on trigger
+    //[SerializeField] GameObject objectPrefabToSpawn;
+    //[SerializeField] float objectMoveSpeed = 1.0f;
+    //[SerializeField] Transform startMarker;
+    //[SerializeField] Transform endMarker;
+
+    void Update()
+    {
+        //timeSinceLastTrigger += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" || collision.gameObject.name == "Player(Clone)")
+        {
+            SpikeToChange.Active = !SpikeToChange.Active;
+            SpikeToChange2.Active = !SpikeToChange2.Active;
+        }
+    }
+}
---
+++ Project Dungeon/Assets/Scipts/Spike.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public bool Active;
+    [SerializeField] int Damage;
+    [SerializeField] int spriteVersion = 0;
+    //private SpriteRenderer spriteR;
+    private BoxCollider2D boxCollider2D;
+    public Animator animator;
+    
+    void Start()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var spikeSprite = Resources.Load<Sprite>("Dungeon_Tileset_214");
+        //var noSpikeSprite = Resources.Load<Sprite>("Dungeon_Tileset_216");
+        if (Active)
+        {
+            //spriteR.sprite = spikeSprite;
+            animator.SetBool("spikeActive", true);
+            boxCollider2D.enabled = true;
+        }
+        else
+        {
+            //spriteR.sprite = noSpikeSprite;
+            animator.SetBool("spikeActive", false);
+            boxCollider2D.enabled = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.gameObject.name == "Player" || collision.gameObject.name == "Player(Clone)")&& Active)
+        {
+            collision.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(Damage); ;
+            
+        }
+    }
+}
