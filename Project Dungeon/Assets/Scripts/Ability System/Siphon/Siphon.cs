fdd4d799d821fd5fd4e7ab732488ff9390cdb444
HaloSeventy7 <njmalenfant@gmail.com>
2019-05-08 14:50:49 -0400

    Created the siphon ability

--- Project Dungeon/Assets/Scripts/Ability System/Siphon/Siphon.cs
+++ Project Dungeon/Assets/Scripts/Ability System/Siphon/Siphon.cs
@@ -8,29 +8,46 @@ public class Siphon : MonoBehaviour //make sure to attach to the player!
     [HideInInspector] public int healthToReturn;
 
     PlayerHealthManager playerHealthManager;
-    int maxHealth;
-    int currentHealth;
+    HurtEnemy hurtEnemy;
+
+    private int maxHealth;
+    private int currentHealth;
+    private bool isSiphonActive = false;
 
     public void Setup() //performs the same actions as MonoBehaviour's Start() function
     {
         playerHealthManager = FindObjectOfType<PlayerHealthManager>();
+        hurtEnemy = FindObjectOfType<HurtEnemy>();
+
         maxHealth = playerHealthManager.GetMaxHealth();
     }
 
     public void Update()
     {
         currentHealth = playerHealthManager.GetCurrentHealth();
+
+        if (hurtEnemy.GetSuccessfulHit()) //checks if the hit was successful after triggering siphon
+        {
+            if (currentHealth + healthToReturn >= maxHealth) //if returning health would make the players health greater than 100, set the current health to 100
+            {
+                playerHealthManager.SetCurrentHealth(maxHealth);
+            }
+            else
+            {
+                playerHealthManager.SetCurrentHealth(currentHealth + healthToReturn);
+            }
+            hurtEnemy.SetSuccessfulHit();
+            isSiphonActive = false;
+        }
     }
 
     public void PerformSiphon()
     {
-        if (currentHealth + healthToReturn >= maxHealth) //if returning health would make the players health greater than 100, set the current health to 100
-        {
-            playerHealthManager.SetCurrentHealth(maxHealth);
-        }
-        else
-        {
-            playerHealthManager.SetCurrentHealth(currentHealth + healthToReturn);
-        }
+        isSiphonActive = true;
+    }
+
+    public bool GetIsSiphonActive()
+    {
+        return isSiphonActive;
     }
 }
--- Project Dungeon/Assets/Scripts/HurtEnemy.cs
+++ Project Dungeon/Assets/Scripts/HurtEnemy.cs
@@ -8,14 +8,37 @@ public class HurtEnemy : MonoBehaviour
     [SerializeField] GameObject damageBurst;
     [SerializeField] Transform hitPoint;
 
+    Siphon siphon;
+
+    private bool successfulHit = false;
+
+    void Start()
+    {
+        siphon = FindObjectOfType<Siphon>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Enemy")
         {
             //Destroy(other.gameObject);
+            if (siphon.GetIsSiphonActive()) //checks if siphon was recently triggered
+            {
+                successfulHit = true; //the hit was successfully made
+            }
             other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToDeal);
             GameObject tempBurst = Instantiate(damageBurst, other.transform.position, other.transform.rotation);
             Object.Destroy(tempBurst, 1.0f);
         }
+    }
+
+    public bool GetSuccessfulHit() //returns whether the hit was successful or not
+    {
+        return successfulHit;
+    }
+
+    public void SetSuccessfulHit() //sets back to false
+    {
+        successfulHit = false;
     }
 }
