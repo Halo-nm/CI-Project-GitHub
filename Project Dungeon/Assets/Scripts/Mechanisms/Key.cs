7cc50fa0720663ccc3d2c3adf6c2cc0ec1f73d43
HaloSeventy7 <njmalenfant@gmail.com>
2019-05-13 19:53:57 -0400

    Began work on a level, created key and lock scripts

---
+++ Project Dungeon/Assets/Scripts/Mechanisms/Key.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] GameObject entryToDestroy; //optional
+    [SerializeField] bool multipleKeysRequired = false;
+
+    MultiKeyLock multiKeyLock;
+
+    void Start()
+    {
+        multiKeyLock = FindObjectOfType<MultiKeyLock>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (!multipleKeysRequired && entryToDestroy != null) //if multiple keys aren't required to destroy the entry, then simply destroy the entry now
+            {
+                Destroy(entryToDestroy);
+                Destroy(gameObject); //destroy this key
+            }
+            else
+            {
+                multiKeyLock.UpdateKeysRequired(gameObject); //tests if the required keys array list needs to be updated
+                Destroy(gameObject); //destroy this key
+            } 
+        }
+    }
+}
