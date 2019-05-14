de73ed63e84f88d7d9d2fa72c2eba7c13c8830c5
HaloSeventy7 <njmalenfant@gmail.com>
2019-05-14 10:38:28 -0400

    Added potions

--- Project Dungeon/Assets/Scripts/Health/HealthCollectable.cs
+++ Project Dungeon/Assets/Scripts/Health/HealthCollectable.cs
@@ -8,17 +8,34 @@ public class HealthCollectable : MonoBehaviour
 
     PlayerHealthManager playerHealthManager;
 
+    private int maxHealth;
+    private int currentHealth;
+
     void Start()
     {
         playerHealthManager = FindObjectOfType<PlayerHealthManager>();
+
+        maxHealth = playerHealthManager.GetMaxHealth();
+    }
+
+    void Update()
+    {
+        currentHealth = playerHealthManager.GetCurrentHealth();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            if (currentHealth + healthToReturn >= maxHealth) //if returning health would make the players health greater than 100, set the current health to 100
+            {
+                playerHealthManager.SetCurrentHealth(maxHealth);
+            }
+            else
+            {
+                playerHealthManager.SetCurrentHealth(currentHealth + healthToReturn);
+            }
             Destroy(gameObject);
-            playerHealthManager.SetCurrentHealth(playerHealthManager.GetCurrentHealth() + healthToReturn);
         }
     }
 }
