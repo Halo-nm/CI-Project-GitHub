338f636e49f60689c3df9d04fde4afbcf4358123
HaloSeventy7 <njmalenfant@gmail.com>
2019-05-20 22:42:39 -0400

    Added enemy health bars

--- Project Dungeon/Assets/Scripts/Health/EnemyHealthBarManager.cs
+++ Project Dungeon/Assets/Scripts/Health/EnemyHealthBarManager.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class EnemyHealthBarManager : MonoBehaviour //attached to the enemy
 {
-    public Slider healthBar;
+    //public Slider healthBar;
+
+    public Vector3 localScale;
 
     private float maxHealth;
     private float currentHealth; //both these variables are set to floats so they don't round to "0" when calculating the enemy's current health
@@ -14,14 +16,18 @@ public class EnemyHealthBarManager : MonoBehaviour //attached to the enemy
 
     void Start()
     {
-        enemyHealthManager = GetComponent<EnemyHealthManager>(); //get a reference to the EnemyHealthManager script that is ATTACHED to this object, hence why it's "GetComponent"
+        enemyHealthManager = GetComponentInParent<EnemyHealthManager>(); //get a reference to the EnemyHealthManager script that is ATTACHED to this object, hence why it's "GetComponent"
+        maxHealth = enemyHealthManager.GetMaxHealth(); //sets a variable to the value of the enemy's max health
+        localScale.x = maxHealth / 1000; //divided by 1000 because the sprite is very big
     }
 
     void Update()
     {
-        maxHealth = enemyHealthManager.GetMaxHealth(); //sets a variable to the value of the enemy's max health
         currentHealth = enemyHealthManager.GetCurrentHealth(); //sets a variable to teh value of the enemy's max health
-        healthBar.maxValue = maxHealth;
-        healthBar.value = currentHealth;
+        localScale.x = currentHealth / 1000;
+        transform.localScale = new Vector3 (localScale.x, 0.01f, 0); //sets the size of the health bar based on the enemies current health
+
+        //healthBar.maxValue = maxHealth;
+        //healthBar.value = currentHealth;
     }
 }
